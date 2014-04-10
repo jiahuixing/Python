d1711214d1810b99e8c13b83f141d44602c323bd
jiahuixing <jiahuixing@gmail.com>
2014-04-10 11:17:31 +0800

    Operate phone

--- flashPhone/Operate.py
+++ flashPhone/Operate.py
@@ -25,6 +25,34 @@ class Operate:
         root_devices(device_list)
         remount_devices(device_list)
 
+    def push_file(self, file_name):
+        device_list = self.adb_device_list
+        work_path = '/home/jiahuixing/music'
+        os.chdir(work_path)
+        push_command = 'adb -s %s push '
+        push_path = ' /data/system/'
+        reboot_command = 'adb -s %s reboot'
+        for device in device_list:
+            if os.path.exists(file_name):
+                debug(device)
+                debug('file exists.')
+                push_cmd = (push_command % device) + file_name + push_path
+                reboot_cmd = reboot_command % device
+                os.system(push_cmd)
+                os.system(reboot_cmd)
+
+    def delete_file(self, file_name):
+        delete_command = 'adb -s %s shell rm '
+        reboot_command = 'adb -s %s reboot'
+        file_path = '/data/system/'
+        device_list = self.adb_device_list
+        for device in device_list:
+            debug(device)
+            delete_cmd = (delete_command % device) + file_path + file_name
+            reboot_cmd = reboot_command % device
+            os.system(delete_cmd)
+            os.system(reboot_cmd)
+
     def push_files(self):
         device_list = self.adb_device_list
         file_path = File_Path
@@ -44,16 +72,19 @@ class Operate:
 
 try:
     op = Operate()
+    op.root_and_remount()
     info = '''
 Input num:
-1.root_and_remount
+1.push_file:%s
+2.delete_file:%s
     '''
-    input_num = input(info)
+    local_file_name = 'account_preview'
+    input_num = input(info % (local_file_name, local_file_name))
     if isinstance(input_num, int):
         if input_num == 1:
-            op.root_and_remount()
-        # elif input_num == 2:
-        #     op.push_files()
+            op.push_file(local_file_name)
+        elif input_num == 2:
+            op.delete_file(local_file_name)
         else:
             print('Input wrong num:%s.' % input_num)
     else:
--- flashPhone/flash_phone_test.py
+++ flashPhone/flash_phone_test.py
@@ -22,16 +22,18 @@ def test3():
     file_path = Tmp_File_Path[1]
     debug(file_path)
     root = ET.parse(file_path)
-    tag = 'commands'
-    nodes = root.findall(tag)
-    debug(type(nodes))
-    debug(nodes)
-    for node in list(nodes):
-        assert isinstance(node, ET.Element)
-        debug('tag=%s,attr=%s' % (node.tag, node.attrib))
-        # for key in Attr_s.keys():
-        #     attr = Attr_s[key]
-        #     debug('attr=%s' % attr)
+    commands_tag = 'commands'
+    commands_nodes = root.findall(commands_tag)
+    debug(type(commands_nodes))
+    debug(commands_nodes)
+    for commands_node in list(commands_nodes):
+        assert isinstance(commands_node, ET.Element)
+        debug('tag=%s,attr=%s' % (commands_node.tag, commands_node.attrib))
+        command_tag = 'command'
+        command_nodes = commands_node.findall(command_tag)
+        for command_node in list(command_nodes):
+            assert isinstance(command_node, ET.Element)
+            debug('tag=%s,attr=%s,text=%s' % (command_node.tag, command_node.attrib, command_node.text))
 
 
 test3()
