051cb7248850f5bd451990cbcc50cba142885ee9
jiahuixing <jiahuixing@gmail.com>
2014-05-15 20:37:13 +0800

    flash phone

--- flashPhone/flashphone.py
+++ flashPhone/flashphone.py
@@ -261,8 +261,16 @@ class FlashPhone:
         except TypeError:
             print('TypeError')
 
-    def test(self):
-        threading.Thread(target=self.input_msg(0))
+# import threading
+#
+# def input_func( context ):
+#     context[ 'data' ] = input( 'input:' )
+#
+# context = { 'data' : 'default' }
+# t = threading.Thread( target = input_func ,args = ( context , ) )
+# t.start( )
+# t.join( 10 )        #等待10秒
+# print( context )
 
 
 if __name__ == '__main__':
