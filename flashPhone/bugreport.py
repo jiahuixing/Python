1a20be88de03c2295f6cba56ffbd1e7736a51e7c
jiahuixing <jiahuixing@gmail.com>
2014-05-20 17:55:46 +0800

    bugreport

--- flashPhone/bugreport.py
+++ flashPhone/bugreport.py
@@ -7,11 +7,18 @@ from libs import *
 root_path = '/home/jiahuixing'
 bug_path = 'bugreport'
 
+bug_suffix = '.txt'
+tgz_suffix = '.tar.gz'
+
 
 class Bugreport:
+    work_path = ''
+    file_name = ''
+    txt_name = ''
     adb_device_list = list()
 
     def __init__(self):
+        self.work_path = '%s/%s/' % (root_path, bug_path)
         adb_permission()
 
     def adb_devices_list(self):
@@ -28,32 +35,36 @@ class Bugreport:
         self.adb_device_list = sorted(adb_device_list)
 
     def get_bugreport(self):
-        work_path = '%s/%s/' % (root_path, bug_path)
-        debug_msg(work_path)
-        os.chdir(root_path)
-        if not os.path.exists(bug_path):
-            os.mkdir(bug_path)
-        for i in xrange(len(self.adb_device_list)):
-            device = self.adb_device_list[i][0]
-            now = time.strftime('%H_%M_%S')
-            tpm = '%s_%s' % (get_date(), now)
-            name = 'bugreport-%s-%s' % (device, tpm)
-            bug_suffix = '.txt'
-            txt_name = '%s%s' % (name, bug_suffix)
-            debug_msg(color_msg(txt_name))
-            cmd = 'adb -s %s bugreport > %s%s' % (device, work_path, txt_name)
-            debug_msg(cmd)
-            os.system(cmd)
-            tgz_suffix = '.tar.gz'
-            tgz_name = '%s%s' % (name, tgz_suffix)
-            debug_msg(color_msg(tgz_name))
-            os.chdir(work_path)
-            cmd = 'tar -zcvf %s %s%s' % (tgz_name, name, bug_suffix)
-            debug_msg(cmd)
-            os.system(cmd)
-            cmd = 'rm -rf %s' % txt_name
-            debug_msg(cmd)
-            os.system(cmd)
+        if len(self.adb_device_list) > 0:
+            debug_msg(self.work_path)
+            os.chdir(root_path)
+            if not os.path.exists(bug_path):
+                os.mkdir(bug_path)
+            for i in xrange(len(self.adb_device_list)):
+                device = self.adb_device_list[i][0]
+                now = time.strftime('%H_%M_%S')
+                tpm = '%s-%s' % (get_date(), now)
+                self.file_name = 'bugreport-%s-%s' % (device, tpm)
+                self.txt_name = '%s%s' % (self.file_name, bug_suffix)
+                debug_msg(color_msg(self.txt_name))
+                cmd = 'adb -s %s bugreport > %s%s' % (device, self.work_path, self.txt_name)
+                debug_msg(cmd)
+                os.system(cmd)
+                self.tar_file()
+        else:
+            print('adb device not found.')
+
+    # noinspection PyMethodMayBeStatic
+    def tar_file(self):
+        tgz_name = '%s%s' % (self.file_name, tgz_suffix)
+        debug_msg(color_msg(tgz_name))
+        os.chdir(self.work_path)
+        cmd = 'tar -zcvf %s %s%s' % (tgz_name, self.file_name, bug_suffix)
+        debug_msg(cmd)
+        os.system(cmd)
+        cmd = 'rm -rf %s' % self.txt_name
+        debug_msg(cmd)
+        os.system(cmd)
 
 
 if __name__ == '__main__':
