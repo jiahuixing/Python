64315b6399f780a0c1ff6e85bf149437d2f2b311
jiahuixing <jiahuixing@gmail.com>
2014-06-25 17:41:09 +0800

    bugreport

--- flashPhone/bugreport.py
+++ flashPhone/bugreport.py
@@ -39,17 +39,12 @@ class Bugreport:
     def get_bugreport(self):
         debug_msg(color_msg('------get_bugreport------', RED))
         if len(self.adb_device_list) > 0:
-            # debug_msg(self.work_path)
-            # os.chdir(root_path)
-            # if not os.path.exists(bug_path):
-            #     os.mkdir(bug_path)
             for i in xrange(len(self.adb_device_list)):
                 device = self.adb_device_list[i][0]
                 now = time.strftime('%H_%M_%S')
                 tpm = '%s-%s' % (get_date(), now)
                 self.file_name = 'bugreport-%s-%s' % (device, tpm)
                 self.txt_name = '%s%s' % (self.file_name, bug_suffix)
-                # debug_msg(color_msg(self.txt_name))
                 cmd = 'adb -s %s bugreport > %s%s' % (device, self.work_path, self.txt_name)
                 color_cmd = 'adb -s %s bugreport > %s%s' % (device, self.work_path, color_msg(self.txt_name))
                 debug_msg(color_cmd)
@@ -63,18 +58,14 @@ class Bugreport:
     def tar_file(self):
         debug_msg(color_msg('------tar_file------', RED))
         self.tgz_name = '%s%s' % (self.file_name, tgz_suffix)
-        # debug_msg(color_msg(self.tgz_name))
-        # os.chdir(self.work_path)
-        # cmd = 'tar -zcvf %s %s%s' % (self.tgz_name, self.file_name, bug_suffix)
-        # color_cmd = 'tar -zcvf %s %s%s' % (color_msg(self.tgz_name), self.file_name, bug_suffix)
         cmd = 'tar -zcf %s %s%s' % (self.tgz_name, self.file_name, bug_suffix)
         color_cmd = 'tar -zcf %s %s%s' % (color_msg(self.tgz_name), self.file_name, bug_suffix)
         debug_msg(color_cmd)
         os.system(cmd)
-        cmd = 'rm -rf %s' % self.txt_name
-        color_cmd = 'rm -rf %s' % color_msg(self.txt_name)
-        debug_msg(color_cmd)
-        os.system(cmd)
+        # cmd = 'rm -rf %s' % self.txt_name
+        # color_cmd = 'rm -rf %s' % color_msg(self.txt_name)
+        # debug_msg(color_cmd)
+        # os.system(cmd)
 
     def push_tgz(self):
         debug_msg(color_msg('------push_tgz------', RED))
@@ -94,6 +85,10 @@ class Bugreport:
             cmd = 'rm -rf %s' % self.tgz_name
             color_cmd = 'rm -rf %s' % color_msg(self.tgz_name)
             debug_msg(color_cmd)
+            os.system(cmd)
+            cmd = 'sudo cp -rf %s %s' % (self.txt_name, svn_path)
+            color_cmd = 'sudo cp -rf %s %s' % (color_msg(self.txt_name), svn_path)
+            debug_msg(color_msg(color_cmd))
             os.system(cmd)
 
 
